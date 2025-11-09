e99e102b0374a163087da717a1600ebedfedf363
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2025-11-09 16:42:14 +0100

    removed astages

--- src/common/files/kubejs/server_scripts/main.js
+++ src/common/files/kubejs/server_scripts/main.js
@@ -4,13 +4,6 @@ let $HashMap = Java.loadClass("java.util.HashMap")
 // Visit the wiki for more info - https://kubejs.com/
 console.info('Hello, World! (Loaded server example script)')
 
-AStages.customizeStage('test_stage_1')
-    .setAddTitle('stage 1 unlocked')
-
-
-AStages.addRestrictionForMod("remove_vanilla_items_0", "test_stage_1", "minecraft")
-    .ignoreItems("minecraft:dirt", "minecraft:grass_block")
-// Item.of('acacia_button')
 
 let map = new $HashMap();
 
@@ -35,14 +28,5 @@ ServerEvents.recipes(event => {
 ServerEvents.tags('item', event => {
     console.info("Run tags")
 })
-
-// Server Script Folder -> file.js (arbitrary file name).
-// AStages.addRestrictionForItem("astages/item1", "test_stage_1", Items.OAK_LOG, "chest")
-// AStages.addRestrictionForTag("astages/item2", "test_stage_1", "forge:ingots/iron")
-// AStages.addRestrictionForMod("astages/item3", "test_stage_1", "minecraft")
-// AStages.addRestrictionForArmor("astages/item4", "test_stage_1", "diamond_helmet", "diamond_chestplate")
-
-// AModels.createPredicateModel("astages:rarity", stack => stack.rarity == $Rarity.EPIC) // ONLY this line MUST be copied in a client script file!
-// AStages.addRestrictionForPredicate("astages/item5", "test_stage_1", "astages:rarity")
 
 console.info("TestABC")
