16b4dbce6b7c8cbe06dca46acbeafd024eb84b82
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-12-09 19:38:31 +0100

    game stages

--- src/common/files/kubejs/server_scripts/item_stages.js
+++ src/common/files/kubejs/server_scripts/item_stages.js
@@ -7,3 +7,11 @@
 function registerStage(name, items) {
     console.info("Register stage " + name)
 }
+
+StageEvents.register(event => {
+    let iron_age = event.registerStage("iron_age")
+    let m =event.mods("create", "create_new_age", "createaddition", "minecraft", "exdeorum", "ftbquests", "woodenshears")
+    event.restrictItems(iron_age, m)
+    event.restrictFluids(iron_age, m)
+})
+
