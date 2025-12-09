a92eafafe1fc0b0ba54aae61657f7b995c545bca
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-12-09 19:39:55 +0100

    game stages

--- src/common/files/kubejs/server_scripts/item_stages.js
+++ src/common/files/kubejs/server_scripts/item_stages.js
@@ -8,9 +8,27 @@ function registerStage(name, items) {
     console.info("Register stage " + name)
 }
 
+/**
+ * @template {object} T
+ * @param {T} event
+ * @returns {Pick<T, { [K in keyof T]: T[K] extends Function ? K : never }[keyof T]>}
+ */
+function destructurable(event) {
+    /** @type {any} */
+    const out = {};
+    for (const key in event) {
+        if (typeof event[key] === "function") {
+            out[key] = event[key].bind(event);
+        }
+    }
+    return out;
+}
+
+
 StageEvents.register(event => {
+    const { mods } = destructurable(event)
     let iron_age = event.registerStage("iron_age")
-    let m =event.mods("create", "create_new_age", "createaddition", "minecraft", "exdeorum", "ftbquests", "woodenshears")
+    let m = mods("create", "create_new_age", "createaddition", "minecraft", "exdeorum", "ftbquests", "woodenshears")
     event.restrictItems(iron_age, m)
     event.restrictFluids(iron_age, m)
 })
