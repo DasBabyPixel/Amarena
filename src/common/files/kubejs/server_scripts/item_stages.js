a6182be425b22d4286fc5733f1b22af09a79253f
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2026-05-14 13:55:05 +0200

    update gamestages

--- src/common/files/kubejs/server_scripts/item_stages.js
+++ src/common/files/kubejs/server_scripts/item_stages.js
@@ -1,30 +1,5 @@
 // priority:9999
 
-/**
- * @param {String} name 
- * @param {(import("net.minecraft.world.item.ItemStack").$ItemStack$$Type | `#${Special.ItemTag}`)[]} items 
- */
-function registerStage(name, items) {
-    console.info("Register stage " + name)
-}
-
-/**
- * @template {object} T
- * @param {T} event
- * @returns {Pick<T, { [K in keyof T]: T[K] extends Function ? K : never }[keyof T]>}
- */
-function destructurable(event) {
-    /** @type {any} */
-    const out = {};
-    for (const key in event) {
-        if (typeof event[key] === "function") {
-            out[key] = event[key].bind(event);
-        }
-    }
-    return out;
-}
-
-
 StageEvents.register(event => {
     const { mods } = destructurable(event)
     let iron_age = event.registerStage("iron_age")
